be219de0483cced28e3306bf442599e80d4b698f
Sienna <sgoldenbird@gmail.com>
2025-03-11 22:53:18 +0900

    solve: kids with the greatest number of candies

--- easy/kids_with_the_greatest_number_of_candies.js
+++ easy/kids_with_the_greatest_number_of_candies.js
@@ -6,3 +6,9 @@ source: LeetCode 75
 number: 1431
 link:https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/?envType=study-plan-v2&envId=leetcode-75
 */
+
+const kidsWithCandies = function (candies, extraCandies) {
+  const maxCandies = Math.max(...candies);
+
+  return candies.map((candy) => candy + extraCandies >= maxCandies);
+};
