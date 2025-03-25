30e5e32ee608023f0c1af1315849b4e0421a5a05
Sienna <sgoldenbird@gmail.com>
2025-03-25 22:59:18 +0900

    solve: reverse vowels of a string

--- sienna/easy/Reverse Vowels of a String.js
+++ sienna/easy/Reverse Vowels of a String.js
@@ -7,4 +7,23 @@ number: 345
 link: https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
 */
 
-var reverseVowels = function (s) {};
+var reverseVowels = function (string) {
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  let arr = string.split("");
+  let left = 0,
+    right = string.length - 1;
+
+  while (left < right) {
+    if (!vowels.has(arr[left])) {
+      left++;
+    } else if (!vowels.has(arr[right])) {
+      right--;
+    } else {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+
+  return arr.join("");
+};
