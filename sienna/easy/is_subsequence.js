fb3285b7265d00ba83e218cd60e9e8397a720a73
Sienna <sgoldenbird@gmail.com>
2025-03-31 01:40:42 +0900

    solve: find the highest altitude

---
+++ sienna/easy/is_subsequence.js
@@ -0,0 +1,8 @@
+/* 
+date: 2025-03-28
+category: 
+difficulty: easy 
+source: LeetCode 75
+number: 392
+link: https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+*/
---
+++ sienna/easy/maximum_average_subarray1.js
@@ -0,0 +1,8 @@
+/* 
+date: 2025-03-28
+category: 
+difficulty: easy 
+source: LeetCode 75
+number: 643
+link: https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75
+*/
