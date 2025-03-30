93219040893b0b22ecfc13c4967e517bea13bb85
Sienna <sgoldenbird@gmail.com>
2025-03-31 01:52:55 +0900

    solve: is subsequence

--- sienna/easy/is_subsequence.js
+++ sienna/easy/is_subsequence.js
@@ -6,3 +6,16 @@ source: LeetCode 75
 number: 392
 link: https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
 */
+var isSubsequence = function (s, t) {
+  let i = 0,
+    j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+
+  return i === s.length;
+};
