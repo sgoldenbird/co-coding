c506d3281aa2be463b8495ec0af22b63ffbea97c
Sienna <sgoldenbird@gmail.com>
2025-04-05 01:48:58 +0900

    solve: find difference of two arrays

--- sienna/easy/problems/find_difference_of_two_arrays.js
+++ sienna/easy/problems/find_difference_of_two_arrays.js
@@ -1,9 +1,17 @@
 /* 
 date: 2025-04-03
-category: array
+category: array, set
 difficulty: easy 
 source: LeetCode 75
 number: 2215
 link: https://leetcode.com/problems/find-the-difference-of-two-arrays/description/?envType=study-plan-v2&envId=leetcode-75
 */
-var findDifference = function (nums1, nums2) {};
+var findDifference = function (nums1, nums2) {
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+
+  const onlyInNums1 = [...set1].filter((num) => !set2.has(num));
+  const onlyInNums2 = [...set2].filter((num) => !set1.has(num));
+
+  return [onlyInNums1, onlyInNums2];
+};
