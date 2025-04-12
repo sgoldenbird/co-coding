c3a877f5e93cb748945b8173fddced582847974e
Sienna <sgoldenbird@gmail.com>
2025-04-13 00:32:07 +0900

    solve: reverse linked list

--- sienna/easy/explanation/reverse_linked_list.js
+++ sienna/easy/explanation/reverse_linked_list.js
@@ -19,15 +19,16 @@ link: https://leetcode.com/problems/reverse-linked-list/?envType=study-plan-v2&e
  * @return {ListNode}
  */
 var reverseList = function (head) {
-  let prev = null;
-  let curr = head;
+  //curr이 null이 될 때까지, 즉 리스트 끝에 도달할 때까지 반복
+  let prev = null; // 처음엔 이전 노드가 없음(null)
+  let curr = head; // 현재 노드는 head부터 시작
 
   while (curr !== null) {
-    let next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+    let next = curr.next; // 다음 노드를 저장해두기
+    curr.next = prev; // 현재 노드의 방향을 반대로 바꾸기
+    prev = curr; // prev를 현재 노드로 업데이트
+    curr = next; // 다음 노드로 이동
   }
 
-  return prev;
+  return prev; // prev는 뒤집힌 리스트의 새 head
 };
